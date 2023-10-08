833401a08ae42d5e22886dfa8e8784b431b464ad
AgnikaR <agnika@codebuddy.co>
2023-10-08 23:33:06 +0530

    paypal demo setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'payment-demo';
+  amount = 40;
+
+  @ViewChild('paymentRef', { static: true }) paymentRef!: ElementRef;
+
+  ngOnInit(): void {
+    // this.amount = this.payment.totalAmount;
+    console.log(window.paypal);
+    window.paypal
+      .Buttons({
+        style: {
+          layout: 'horizontal',
+          color: 'blue',
+          shape: 'rect',
+          label: 'paypal',
+        },
+        createOrder: (data: any, actions: any) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: this.amount.toString(),
+                  currency_code: 'USD',
+                },
+              },
+            ],
+          });
+        },
+        onApprove: (data: any, actions: any) => {
+          return actions.order.capture().then((details: any) => {
+            if (details.status === 'COMPLETED') {
+              console.log('completed status', details);
+              // this.payment.transactionID = details.id;
+              // this.router.navigate(['confirm']);
+            }
+          });
+        },
+        onError: (error: any) => {
+          console.log(error);
+        },
+      })
+      .render(this.paymentRef.nativeElement);
+  }
+
+  // cancel() {
+  //   this.router.navigate(['cart']);
+  // }
 }
